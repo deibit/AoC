d85fd44c418d19ea906ae2bf1207d77afba45016
deibit <daganu@gmail.com>
2023-12-10 20:22:33 +0100

    day 8 part 2

--- 2023/08/2.py
+++ 2023/08/2.py
@@ -1,8 +1,28 @@
+"""
+
+I really dislike how this problem has to be solved in Part.2
+
+Nevertheless, its very instructive and as usually fun to solve.
+
+Every **A has a cycle. You have to detect when this cycle happens and 
+
+calculate the lcm of all of them. 
+
+This is the iteration where all the **A would be finish on a **Z.
+
+So, its not solved by carefully reading the statement.
+
+It's also not solved by skimming throught the input.
+
+It is solved by inspecting the output and figuring out there is a cycle.
+
+"""
+
+
+import math
 import re
 
 lines = open("input.txt").readlines()
-
-
 inst = list(lines[0].strip())
 
 
@@ -20,26 +40,6 @@ for line in lines[2:]:
     nodes[node.name] = node
 
 
-# A_nodes = [node for node in nodes.values() if node.name.endswith("A")]
-# step = 0
-# a_node = A_nodes[0]
-# cycle = []
-# print(f"solving cycle for {a_node.name}")
-# while True:
-#     current_direction = inst[step % len(inst)]
-#     step += 1
-#     match current_direction:
-#         case "L":
-#             cycle.append((a_node.name, "L"))
-#             a_node = nodes[a_node.left]
-#         case "R":
-#             cycle.append((a_node.name, "R"))
-#             a_node = nodes[a_node.right]
-#     if len(cycle) > len(set(cycle)):
-#         print(f"Found cycle for with {step}")
-#         for elem in cycle:
-#             print(elem)
-
 results = []
 A_nodes = [node for node in nodes.values() if node.name.endswith("A")]
 l_inst = len(inst)
@@ -49,45 +49,18 @@ for node in A_nodes:
     while True:
         movement = inst[step % l_inst]
         step += 1
-        print(f"iteration: {step} inst: {movement} -> {[n.name for n in A_nodes]}")
+        # print(f"iteration: {step} inst: {movement} -> {[n.name for n in A_nodes]}")
         match movement:
             case "L":
                 current = nodes[current.left]
             case "R":
                 current = nodes[current.right]
         if current.name.endswith("Z"):
-            print(f"found Z for {node.name} with {current.name}")
+            # print(f"found Z for {node.name} with {current.name}")
             break
     results.append((step, current.name))
-print(results)
-quit()
 
-step = 0
-l_inst = len(inst)
-A_nodes = [node for node in nodes.values() if node.name.endswith("A")]
-while not all([n.name.endswith("Z") for n in A_nodes]):
-    movement = inst[step % l_inst]
-    step += 1
-    print(f"iteration: {step} inst: {movement} -> {[n.name for n in A_nodes]}")
-
-    for idx in range(0, len(A_nodes)):
-        current = A_nodes[idx]
-        match movement:
-            case "L":
-                new = nodes[current.left]
-                if not new in A_nodes:
-                    A_nodes[idx] = new
-                else:
-                    A_nodes.remove(new)
-            case "R":
-                new = nodes[current.right]
-                if not new in A_nodes:
-                    A_nodes[idx] = new
-                else:
-                    A_nodes.remove(new)
-            case "_":
-                print("error")
-                quit()
-print([n.name for n in A_nodes])
-
-print(step)
+for_mdc = []
+for result in results:
+    for_mdc.append(result[0])
+print(math.lcm(*for_mdc))
