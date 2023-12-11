8d36fe7caf83ebd65426f48a7b182236d436eedc
deibit <daganu@gmail.com>
2023-12-11 11:15:32 +0100

    day 9 part 1, 2

---
+++ 2023/09/2.py
@@ -0,0 +1,38 @@
+"""
+
+Sh*t, I read "reverse" word in a Reddit thread and instantly came to my mind.
+
+"""
+
+import re
+
+# First "trap". Watch for negative numbers in your regex
+lines = [
+    [int(n) for n in re.findall(r"-?\d+", line)]
+    for line in open("input.txt").readlines()
+]
+
+
+def diff(line):
+    return [line[i + 1] - line[i] for i in range(len(line) - 1)]
+
+
+def predict(lines):
+    lines[-1].append(0)
+    for line in range(len(lines) - 2, -1, -1):
+        lines[line - 1].append(lines[line][-1] + lines[line - 1][-1])
+    return lines[0][-1]
+
+
+results = []
+for current in lines:
+    current.reverse()
+    history = []
+    # Second trap. If this condition check for sum(current) != 0 it will fail because -n + n == 0
+    while sum([abs(n) for n in current]):
+        history.append(current)
+        current = diff(current)
+    history.append(current)
+    results.append(predict(history))
+
+print(sum(results))
