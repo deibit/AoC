f031097fd814655497f4e5450c90e6685e16124e
deibit <daganu@gmail.com>
2023-12-14 19:06:43 +0100

    Day 10 Part 2 (adapted)

--- 2023/10/2.py
+++ 2023/10/2.py
@@ -1,194 +1,121 @@
-from typing import List, Tuple
+"""
 
-from colorama import Fore
+    This solution is not mine. 
+
+    It is an adapted code from 
+
+    https://topaz.github.io/paste/#XQAAAQDYBgAAAAAAAAARmknGRw8TogB3OyW56zkJNAnj1YapbqOnYw6+vS5kUM9H/WrqKcnh8YFzYrKUk5PHWEEt7R3wOd2jnSDhRacpfpoezlT0AqzBOUTzTVZ4WrUECuvW9iqw4MtDoKmyO3Kq+qlVJtOziMBQHNjhPg6ty5F94Eegy3wmqeqy2fq+ZZRRRItk2K7ywvDPqkAf31jySVV4ooWcnuOLUEnjI3ndnvysR8B2CCvDQomo3Oxvzh1e+l3W1PYFqkXWAi1VV3voNpg+6tTC5vS1cTABDLt7D4ZL5YDRHWvF0J8mRYTzfia1p1fMWZJ7sFD88k6Mem4AdkDwPMpjFkqUGfAfaMmHpepE9IoPvLG2TFBzOLS1JAsvQnJJWVx3zQUFt0RXVfUdFI9k+kHvh91nighnJ6jUILGIQxR2ya9fvnjmDy+dZDA237IvsxcIiTSCXlS2z9r5tBNjt25Hr5vIGqoneFWTlIRExoqgFsQ+/6ExxTGFsrdcue636dMgRaZQLhQKLqjCICeLul/bWPN1ZpKPN+zuMWzj1K8kDp1cMYkyhgvhrPXIjr7QALojx9rNZ+q1BdQ0qcWH6TRJUTJPH2ptGbc0kIXc2c5pCl2R/qcJKZl3/MJkL6bov2c13Cmhjf76Phjk3n+7ga+t0vMWL0qkBSSYYvHbQ1XDyYi1IrIgCboiSf8bSTmpFO8Sn4elc+mlhZ1AK645pByLMMqFo+CdVI/SYOKxpgDhFXOQci25kR5eYTx0UPYJcsSgMVjF/8sC2NUmTqruEsKnLy1J8ShHXMh4S0cWFXjV3/v6YALSPg4JNsTnr1YGf8/CFddOzfko/MWSR9JxqELhtGj/p345Sf9hVx7HWH2TSoxAQP//luxqfA==
+
+  from: https://www.reddit.com/r/adventofcode/comments/18ey1s7/comment/kcrl36f
+
+It is a clever solution further explained by its author here: https://www.reddit.com/r/adventofcode/comments/18f518b/2023_day_10_efficient_approach_to_part_2_math/
+
+The formula is from 
+
+https://en.wikipedia.org/wiki/Shoelace_formula
+
+Didn't like this second part after all.
+
+"""
+
+
+from enum import IntEnum
+
+
+class Direction(IntEnum):
+    None_ = 0
+    Up = 1
+    Down = 2
+    Left = 4
+    Right = 8
+
+
+direction = {
+    "|": Direction.Up | Direction.Down,
+    "-": Direction.Left | Direction.Right,
+    "L": Direction.Up | Direction.Right,
+    "J": Direction.Up | Direction.Left,
+    "7": Direction.Down | Direction.Left,
+    "F": Direction.Down | Direction.Right,
+    ".": Direction.None_,
+    "S": Direction.Up | Direction.Down | Direction.Left | Direction.Right,
+}
 
 FILE = "input.txt"
 
-grid: List[List[str]] = [
-    [col for col in row] for row in [row.strip() for row in open(FILE).readlines()]
-]
-rows = len(grid)
-cols = len(grid[0])
 
-
-class Point:
-    def __init__(self, row, col, level=-1):
-        self.row = row
-        self.col = col
-        self.level = level
-
-    def __str__(self):
-        return f"{self.row}:{self.col} lvl:{self.level} sym:{grid[self.row][self.col]}"
-
-
-def grid_get_point(row, col):
-    return grid[row][col]
-
-
-# I'm too lazy to hardcode starting position
-def start() -> Point:
-    for row in range(0, rows):
-        for col in range(0, cols):
-            if grid_get_point(row, col) == "S":
-                return Point(row, col, 0)
-    quit("[start] Error getting starting position")
-
-
-def check_limits(point: Tuple[int, int]):
-    return 0 <= point[0] < rows and 0 <= point[1] < cols
-
-
-def check_directions(dirs: List[str], point: Point):
-    north, south, west, east = [
-        (point.row - 1, point.col),
-        (point.row + 1, point.col),
-        (point.row, point.col - 1),
-        (point.row, point.col + 1),
+def load():
+    return [
+        [col for col in row] for row in [row.strip() for row in open(FILE).readlines()]
     ]
 
-    feasibles = []
 
-    for dir in dirs:
-        match dir:
-            case "north":
-                if check_limits(north) and grid_get_point(*north) in ["|", "7", "F"]:
-                    feasibles.append(Point(*north))
-            case "south":
-                if check_limits(south) and grid_get_point(*south) in ["|", "L", "J"]:
-                    feasibles.append(Point(*south))
-            case "west":
-                if check_limits(west) and grid_get_point(*west) in ["-", "L", "F"]:
-                    feasibles.append(Point(*west))
-            case "east":
-                if check_limits(east) and grid_get_point(*east) in ["-", "J", "7"]:
-                    feasibles.append(Point(*east))
-    return feasibles
+def main():
+    data = load()
+    r, u = len(data[0]), len(data)
 
+    sx, x, sy, y = 0, 0, 0, 0
+    for k in range(len(data)):
+        for i in range(len(data[k])):
+            if data[k][i] == "S":
+                sx = k
+                x = k
+                sy = i
+                y = i
 
-def surround(point: Point) -> List[Point]:
-    if not point:
-        return []
+    area, circ = 0, 0
+    while True:
+        circ += 1
 
-    symbol = grid[point.row][point.col]
-    feasibles = []
+        # Current symbol
+        c = data[x][y]
 
-    match symbol:
-        case ".":
-            return feasibles
-        case "|":
-            feasibles.extend(check_directions(["north", "south"], point))
-        case "-":
-            feasibles.extend(check_directions(["west", "east"], point))
-        case "L":
-            feasibles.extend(check_directions(["north", "east"], point))
-        case "J":
-            feasibles.extend(check_directions(["north", "west"], point))
-        case "7":
-            feasibles.extend(check_directions(["south", "west"], point))
-        case "F":
-            feasibles.extend(check_directions(["south", "east"], point))
-        case "S":
-            feasibles.extend(
-                check_directions(["north", "west", "south", "east"], point)
-            )
+        # Save old x,y to deactivate cell
+        old_x = x
+        old_y = y
 
-    feasibles = [new for new in feasibles if not (new.row, new.col) in visited]
-    for p in feasibles:
-        p.level = point.level + 1
-    return feasibles
+        # Now we search for a route to explore
+        if (  # It means we came from left and want to go right
+            y < r  # Do we have cells to the right?
+            and direction[c] & Direction.Right
+            and direction[data[x][y + 1]] & Direction.Left
+        ):
+            area += x  # Area is expanding to the right
+            y += 1  # Also y is expanding
+        elif (
+            x < u
+            and direction[c] & Direction.Down
+            and direction[data[x + 1][y]] & Direction.Up
+        ):
+            area -= y
+            x += 1
+        elif (
+            y > 0
+            and direction[c] & Direction.Left
+            and direction[data[x][y - 1]] & Direction.Right
+        ):
+            area -= x
+            y -= 1
+        elif (
+            x > 0
+            and direction[c] & Direction.Up
+            and direction[data[x - 1][y]] & Direction.Down
+        ):
+            area += y
+            x -= 1
 
+        # At this point there is no route to go further
+        else:
+            break
 
-visited = []
-pending = [start()]
+        # Deactivate cell
+        data[old_x][old_y] = "."
 
+    area += x * sy - y * sx
 
-while pending:
-    next = pending[0]
-    pending = pending[1:]
-
-    if (next.row, next.col) in visited:
-        print(f"[FOUND END] {next}")
-        break
-
-    pending.extend(surround(next))
-    visited.append((next.row, next.col))
-
-
-def check_directions2(point):
-    row = point[0]
-    col = point[1]
-    pointeables = [
-        (row - 1, col),
-        (row - 1, col - 1),
-        (row - 1, col + 1),
-        (row + 1, col),
-        (row + 1, col - 1),
-        (row + 1, col + 1),
-        (row, col - 1),
-        (row, col + 1),
-    ]
-
-    feasibles = []
-    for pointeable in pointeables:
-        if check_limits(pointeable):
-            feasibles.append(pointeable)
-
-    return feasibles
+    print("Part 1:", circ // 2)
+    print("Part 2:", (abs(area) - circ) // 2 + 1)
 
 
-grid.append(["." for _ in range(0, len(grid[0]))])
-grid.reverse()
-grid.append(["." for _ in range(0, len(grid[0]))])
-grid.reverse()
-for row in grid:
-    row.append(".")
-    row.reverse()
-    row.append(".")
-    row.reverse()
-rows = len(grid)
-cols = len(grid[0])
-
-print("Searching for free points")
-pending = [(0, 0), (0, cols - 1), (rows - 1, cols - 1), (rows - 1, 0)]
-frees = pending[:]
-visited2 = []
-
-while pending:
-    next = pending[0]
-    pending = pending[1:]
-    new_ones = [
-        point
-        for point in check_directions2(next)
-        if not point in visited and not point in pending and not point in visited2
-    ]
-    frees.extend(new_ones)
-    pending.extend(new_ones)
-    visited2.append(next)
-
-print(
-    f"Total: {rows} * {cols} = {rows*cols} The loop: {len(visited)} Free ones: {len(frees)} Inside loop: {(rows*cols)-(len(visited)+len(frees))}"
-)
-print(f"Linea: {len(grid[0])}")
-
-
-c = 0
-
-
-def visualization():
-    global c
-    for row in range(rows):
-        s = ""
-        for col in range(cols):
-            if (row, col) in visited:
-                s += Fore.RED + grid[row][col]
-            elif (row, col) in frees:
-                s += Fore.GREEN + grid[row][col]
-            else:
-                c += 1
-                s += Fore.WHITE + grid[row][col]
-
-        print(s)
-
-
-visualization()
-print(c)
+if __name__ == "__main__":
+    main()
---
+++ 2023/10/2_failed.py
@@ -0,0 +1,195 @@
+from typing import List, Tuple
+
+from colorama import Fore
+
+FILE = "input.txt"
+
+grid: List[List[str]] = [
+    [col for col in row] for row in [row.strip() for row in open(FILE).readlines()]
+]
+rows = len(grid)
+cols = len(grid[0])
+
+
+class Point:
+    def __init__(self, row, col, level=-1):
+        self.row = row
+        self.col = col
+        self.level = level
+
+    def __str__(self):
+        return f"{self.row}:{self.col} lvl:{self.level} sym:{grid[self.row][self.col]}"
+
+
+def grid_get_point(row, col):
+    return grid[row][col]
+
+
+# I'm too lazy to hardcode starting position
+def start() -> Point:
+    for row in range(0, rows):
+        for col in range(0, cols):
+            if grid_get_point(row, col) == "S":
+                return Point(row, col, 0)
+    quit("[start] Error getting starting position")
+
+
+def check_limits(point: Tuple[int, int]):
+    return 0 <= point[0] < rows and 0 <= point[1] < cols
+
+
+def check_directions(dirs: List[str], point: Point):
+    north, south, west, east = [
+        (point.row - 1, point.col),
+        (point.row + 1, point.col),
+        (point.row, point.col - 1),
+        (point.row, point.col + 1),
+    ]
+
+    feasibles = []
+
+    for dir in dirs:
+        match dir:
+            case "north":
+                if check_limits(north) and grid_get_point(*north) in ["|", "7", "F"]:
+                    feasibles.append(Point(*north))
+            case "south":
+                if check_limits(south) and grid_get_point(*south) in ["|", "L", "J"]:
+                    feasibles.append(Point(*south))
+            case "west":
+                if check_limits(west) and grid_get_point(*west) in ["-", "L", "F"]:
+                    feasibles.append(Point(*west))
+            case "east":
+                if check_limits(east) and grid_get_point(*east) in ["-", "J", "7"]:
+                    feasibles.append(Point(*east))
+    return feasibles
+
+
+def surround(point: Point) -> List[Point]:
+    if not point:
+        return []
+
+    symbol = grid[point.row][point.col]
+    feasibles = []
+
+    match symbol:
+        case ".":
+            return feasibles
+        case "|":
+            feasibles.extend(check_directions(["north", "south"], point))
+        case "-":
+            feasibles.extend(check_directions(["west", "east"], point))
+        case "L":
+            feasibles.extend(check_directions(["north", "east"], point))
+        case "J":
+            feasibles.extend(check_directions(["north", "west"], point))
+        case "7":
+            feasibles.extend(check_directions(["south", "west"], point))
+        case "F":
+            feasibles.extend(check_directions(["south", "east"], point))
+        case "S":
+            feasibles.extend(
+                check_directions(["north", "west", "south", "east"], point)
+            )
+
+    feasibles = [new for new in feasibles if not (new.row, new.col) in visited]
+    for p in feasibles:
+        p.level = point.level + 1
+    return feasibles
+
+
+visited = []
+pending = [start()]
+
+
+while pending:
+    next = pending[0]
+    pending = pending[1:]
+
+    if (next.row, next.col) in visited:
+        print(f"[FOUND END] {next}")
+        break
+
+    pending.extend(surround(next))
+    visited.append((next.row, next.col))
+
+
+def check_directions2(point):
+    row = point[0]
+    col = point[1]
+    pointeables = [
+        (row - 1, col),
+        (row - 1, col - 1),
+        (row - 1, col + 1),
+        (row + 1, col),
+        (row + 1, col - 1),
+        (row + 1, col + 1),
+        (row, col - 1),
+        (row, col + 1),
+    ]
+
+    feasibles = []
+    for pointeable in pointeables:
+        if check_limits(pointeable):
+            feasibles.append(pointeable)
+
+    return feasibles
+
+
+grid.append(["." for _ in range(0, len(grid[0]))])
+grid.reverse()
+grid.append(["." for _ in range(0, len(grid[0]))])
+grid.reverse()
+for row in grid:
+    row.append(".")
+    row.reverse()
+    row.append(".")
+    row.reverse()
+rows = len(grid)
+cols = len(grid[0])
+
+print("Searching for free points")
+pending = [(70, 70)]
+# pending = [(0, 0), (0, cols - 1), (rows - 1, cols - 1), (rows - 1, 0)]
+frees = pending[:]
+visited2 = []
+
+while pending:
+    next = pending[0]
+    pending = pending[1:]
+    new_ones = [
+        point
+        for point in check_directions2(next)
+        if not point in visited and not point in pending and not point in visited2
+    ]
+    frees.extend(new_ones)
+    pending.extend(new_ones)
+    visited2.append(next)
+
+print(
+    f"Total: {rows} * {cols} = {rows*cols} The loop: {len(visited)} Free ones: {len(frees)} Inside loop: {(rows*cols)-(len(visited)+len(frees))}"
+)
+print(f"Linea: {len(grid[0])}")
+
+
+c = 0
+
+
+def visualization():
+    global c
+    for row in range(rows):
+        s = ""
+        for col in range(cols):
+            if (row, col) in visited:
+                s += Fore.RED + grid[row][col]
+            elif (row, col) in frees:
+                c += 1
+                s += Fore.GREEN + grid[row][col]
+            else:
+                s += Fore.WHITE + grid[row][col]
+
+        print(s)
+
+
+visualization()
+print(c)
