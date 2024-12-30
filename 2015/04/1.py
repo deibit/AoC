2a7c60b72ff622c55b7f374d3fb1dcfbf464e982
deibit <daganu@gmail.com>
2024-12-30 23:32:33 +0100

    2015

--- 2015/04/1.py
+++ 2015/04/1.py
@@ -5,9 +5,9 @@ from utils.f import readinput
 entries = readinput(__file__)
 
 
-for i in range(1, 10000000):
+for i in range(1, 1000000):
     s = f"{entries}{str(i)}"
     h = hashlib.md5(s.encode()).hexdigest()
-    if h.startswith("000000"):
+    if h.startswith("00000"):
         print(i)
         break
