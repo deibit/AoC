3e3d86b89a73a24782971f583a1f0426d62c3798
deibit <daganu@gmail.com>
2025-01-02 23:04:51 +0100

    2015 10

--- 2015/10/1.py
+++ 2015/10/1.py
@@ -1,3 +1,26 @@
-from utils.f import readinput
+from utils.f import readinput, readtest
 
-entries = readinput(__file__)
+entries = readinput(__file__).split("\n")
+
+# entries = readtest(__file__).split("\n")
+# entries: list[str] = [e for e in entries if e]
+# TIMES = 5
+
+TIMES = 40
+
+seq = entries[0]
+
+for _ in range(TIMES):
+    nseq = ""
+    i = 0
+    while i < len(seq):
+        c = seq[i]
+        p = 1
+        while p + i < len(seq) and seq[i + p] == c:
+            p += 1
+        nseq += f"{p}{c}"
+        i += p
+    seq = nseq
+
+
+print(len(seq))
--- 2015/10/2.py
+++ 2015/10/2.py
@@ -1,3 +1,21 @@
-from utils.f import readinput
+import re
 
-entries = readinput(__file__)
+from utils.f import readinput, readtest
+
+entries = readinput(__file__).split("\n")
+TIMES = 50
+
+# entries = readtest(__file__).split("\n")
+# entries: list[str] = [e for e in entries if e]
+# TIMES = 5
+
+
+# Take a single digit, group its one ore more repetitions
+r = re.compile(r"(\d)\1*")
+
+seq = entries[0]
+
+for _ in range(TIMES):
+    seq = "".join([f"{len(g[0])}{g[0][0]}" for g in re.finditer(r, seq)])
+
+print(len(seq))
